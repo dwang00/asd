419b6d86aa8f370e72c36f1ba5972eaddfee4120
David <dwang00@stuy.edu>
2019-12-17 22:28:42 -0500

    asd

--- shell.h
+++ shell.h
@@ -1,26 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-
-
-
-int count(char* line, char * c);
-
 char **parsein(char* line, char * delim);
-
-int exec_cmd(char *cmd);
 
 int find_redirect(char * line);
 
-int output(char * line);
+int oredirect(char * line);
 
-int inputt(char * line);
+int iredirect(char * line);
 
 int mypipe (char * line) ;
 
 int doubleRedirect(char * line);
-// char *erase(char *p, char * c);
